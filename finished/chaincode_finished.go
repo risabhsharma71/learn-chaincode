121668e1d9650bd497aba8fe50292f6ec0790465
Risabhs <Risabhs@corp.rpqb.com>
2017-04-10 17:32:17 +0530

    some changes

--- finished/chaincode_finished.go
+++ finished/chaincode_finished.go
@@ -77,6 +77,13 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 	fmt.Println("query did not find func: " + function)
 
 	return nil, errors.New("Received unknown function query: " + function)
+	/*	{
+		if function == "Cal_length" { //read a variable
+			return t.Cal_length(stub, args)
+		}
+		fmt.Println("query did not find func: " + function)
+
+	}*/
 }
 
 // write - invoke function to write key/value pair
@@ -117,7 +124,7 @@ func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) 
 	return valAsbytes, nil
 }
 
-func (t *SimpleChaincode) Cal_length(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+/*func (t *SimpleChaincode) Cal_length(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
 	var key, value string
 	var err error
 	fmt.Println("running write()")
@@ -135,4 +142,24 @@ func (t *SimpleChaincode) Cal_length(stub shim.ChaincodeStubInterface, args []st
 		return nil, err
 	}
 	return nil, nil
+}*/
+func (t *SimpleChaincode) Cal_length(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var key, jsonResp string
+	var err error
+
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
+	}
+
+	key = args[0]
+	valAsbytes, err := stub.GetState(key)
+	length := ([]byte(strconv.Itoa(len(valAsbytes))))
+
+	if err != nil {
+		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
+		return nil, errors.New(jsonResp)
+
+	}
+
+	return length, nil
 }
